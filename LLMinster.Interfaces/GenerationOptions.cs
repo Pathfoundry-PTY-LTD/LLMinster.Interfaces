bb2348bdc7914d0e691d41e3a60bfd90c925d20a
Philip Laureano <philip.laureano@gmail.com>
2024-10-04 22:34:16 +1000

    Added initial commit for LLMinster.Interfaces

---
+++ LLMinster.Interfaces/GenerationOptions.cs
@@ -0,0 +1,15 @@
+﻿namespace LLMinster.Interfaces
+{
+    public class GenerationOptions
+    {
+        /// <summary>
+        /// Controls the randomness of the output. Lower values make the output more deterministic.
+        /// </summary>
+        public float Temperature { get; set; } = 0.025f;
+
+        /// <summary>
+        /// The maximum number of tokens to generate.
+        /// </summary>
+        public int MaxTokens { get; set; } = 4096;
+    }
+}
---
+++ LLMinster.Interfaces/ILLMClient.cs
@@ -0,0 +1,8 @@
+﻿using System.Threading.Tasks;
+
+namespace LLMinster.Interfaces;
+
+public interface ILLMClient
+{   
+    Task<string> GenerateContentAsync(string prompt, GenerationOptions options);
+}

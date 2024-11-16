efeaf005d992166836ae5f42913041519d7f28df
Philip Laureano <philip.laureano@gmail.com>
2024-11-16 23:59:34 +1100

    Added GetDirectories method and updated version
    A new method, GetDirectories, has been added to the IDirectorySystem interface. This allows for retrieving all directories within a specified directory path. Additionally, the project version has been incremented from 0.0.2 to 0.0.3 reflecting these changes in functionality.

--- LLMinster.Interfaces/IDirectorySystem.cs
+++ LLMinster.Interfaces/IDirectorySystem.cs
@@ -6,6 +6,8 @@ public interface IDirectorySystem
 {
     string[] GetFiles(string directoryPath, string fileSearchPattern);
     DirectoryInfo CreateDirectory(string directoryPath);
+
+    string[] GetDirectories(string directoryPath);
     void Delete(string directoryPath, bool recursive = false);
     bool Exists(string directoryPath);
 }
